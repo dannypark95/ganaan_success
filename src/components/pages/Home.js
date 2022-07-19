986da5ed046e04d0f97911ba67d3f30e97976e14
Danny Park <dannypark95@gmail.com>
2022-07-19 19:31:18 -0400

    feature: main page updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Boxes from '../main/Boxes';
+
 const Home = () => {
   return (
     <div>
-      <img src={'https://i.imgur.com/viPCWjY.jpg'} alt='Header' />
+      <div className='mainBanner'>
+        <img
+          src={'https://i.imgur.com/QtsL2U1.png'}
+          alt='Header'
+          className='banner'
+        />
+      </div>
+      <div className='middleBanner'>This is Middle Banner</div>
+      <Boxes />
     </div>
   );
 };
