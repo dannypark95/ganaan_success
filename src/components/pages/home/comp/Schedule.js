09c473e7fb27f64c899f2b0f37c341fa6485e86e
Danny Park <dannypark95@gmail.com>
2023-03-18 01:15:02 -0500

    feature: main price centered

--- src/components/pages/home/comp/Schedule.js
+++ src/components/pages/home/comp/Schedule.js
@@ -4,18 +4,26 @@ import campPrice from '../../../../media/home/price.png';
 
 const Schedule = () => {
   return (
-    <div className='row mt-2 container'>
-      <div className='col text-center'>
-        <div className='fw-bolder fs-5 fontMICE'>영어캠프 스케줄</div>
-        <img
-          src={campSchedule}
-          className='homeCampImg'
-          alt='Camp Home Schedule'
-        />
-      </div>
-      <div className='col'>
-        <div className='fw-bolder fs-5 fontMICE'>주니어 영어캠프 비용</div>
-        <img src={campPrice} className='homeCampImg' alt='Camp Home Price' />
+    <div className='text-center'>
+      <div className='d-flex justify-content-center'>
+        <div className='row mt-2 container mx-auto'>
+          <div className='col text-center'>
+            <div className='fw-bolder fs-5 fontMICE'>영어캠프 스케줄</div>
+            <img
+              src={campSchedule}
+              className='homeCampImg'
+              alt='Camp Home Schedule'
+            />
+          </div>
+          <div className='col'>
+            <div className='fw-bolder fs-5 fontMICE'>주니어 영어캠프 비용</div>
+            <img
+              src={campPrice}
+              className='homeCampImg'
+              alt='Camp Home Price'
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
