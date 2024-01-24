6416c3277e7a1e4955190a5986dc254043b79766
“Daniel <dannypark95@gmail.com>
2024-01-24 00:59:50 -0600

    feature: contact info

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
   return (
-    <div className='contact-info'>
-      <div> 주소 : 3rd Floor GCA Bldg, Gov. M. Cuenco Ave, Cebu City </div>
+    <div className="contact-info">
       <div>
-        상담문의 : +63 9267203580 | 홈페이지 : www.cebusuccess.com <br /> 이메일
-        : cebusuccess@gmail.com | 카카오톡 ID : successacademy
+        이메일 : ganaanenglishcamp@gmail.com | 카카오톡 ID : dannypark95
       </div>
-      <div>법인체등록번호 : CS200427256</div>
-      <div>SSP인가 번호 : NO.AFF-05-089</div>
     </div>
   );
 };
