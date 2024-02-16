3a2b4f093fe972344f4f97cb780e0a0383bcaf32
“Daniel <dannypark95@gmail.com>
2024-02-16 00:32:45 -0600

    feature: contact change

--- src/components/layout/Contact.js
+++ src/components/layout/Contact.js
@@ -3,9 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact-info">
-      <div>
-        이메일 : ganaanenglishcamp@gmail.com | 카카오톡 ID : dannypark95
-      </div>
+      <div>이메일 : ganaanenglishcamp@gmail.com | 카카오톡 ID : yjc627</div>
     </div>
   );
 };
